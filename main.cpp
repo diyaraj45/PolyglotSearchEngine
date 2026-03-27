3ed4bb871522c38ddeb2a9f0ddcf4734f18e93cc
Grecia <you@example.com>
2026-03-27 15:48:28 -0400

    Update of main

--- main.cpp
+++ main.cpp
@@ -2,48 +2,63 @@
 #include <string>
 #include <vector>
 #include "parser.h"
+#include "hashtable.h"
+#include "trie_implementation.h"
+#include "timer.h"
 using namespace std;
-
-//temporary fake search functions (replaced by trie later)
-string searchTrie(const vector<entry>& dataset, const string& word) {
-    //Loop through dataset
-    for (int i = 0; i < dataset.size(); i++) {
-        //If the English word matches the search word, return the Spanish translation
-        if (dataset[i].english == word) {
-            return dataset[i].spanish;
-        }
-    }
-    return "Word not found";
-}
-
-//Temporary search using dataset (will be replaced by Hash later)
-string searchHash(const vector<entry>& dataset, const string& word) {
-    //Loop through dataset
-    for (int i = 0; i < dataset.size(); i++) {
-        if (dataset[i].english == word) {
-            return dataset[i].spanish;
-        }
-    }
-    return "Word not found";    
-}
-
 
 int main() {
     //load data from data.scv file
     vector<entry> dataset = loadDataset("data.csv");
+
+    //build trie
+    TrieOperations trieOps;
+    long long trieBuildTime = trieOps.parser_trie(dataset);
+
+    //build hash table
+    hashTable hash(dataset.size());
+
+    Timer hashBuildTimer;
+    hashBuildTimer.start();
+    for (int i = 0; i < dataset.size(); i++) {
+        hash.insert(dataset[i]);
+    }
+
+    long long hashBuildTime = hashBuildTimer.stop();
 
     //user input
     string word;
     cout << "Enter a word to search: ";
     cin >> word;
 
-    //call search functions
-    string trieResult = searchTrie(dataset, word);
-    string hashResult = searchHash(dataset, word);
+    //Trie Search
+    TrieResult trieOutput = trieOps.search_word(word);
+    string trieResult = "Word not found";
+    long long trieTime = trieOutput.time;
+    if (trieOutput.found) {
+        trieResult = trieOutput.translate;
+    }
+    //Hash Search
+    Timer hashSearchTimer;
+    hashSearchTimer.start();
+    entry* hashEntry = hash.search(word);
+    long long hashTime = hashSearchTimer.stop();
+    string hashResult = "Word not found";
 
-    //Temporary timing values (NEED TO REPLACE)
-    long long trieTime = 120;
-    long long hashTime = 80;
+    //check if the hash table found the word
+    if (hashEntry != nullptr) {
+        entry& foundEntry = *hashEntry; //pointer into normal reference.
+        if (!foundEntry.spanish.empty()) { //check for at least one translation
+            string resultText = ""; //store all translations as one string. 
+            for (int i = 0; i < foundEntry.spanish.size(); i++) {
+                resultText += foundEntry.spanish[i];
+                if (i < foundEntry.spanish.size() - 1) { //add comma if not the last translation
+                    resultText += ", ";
+                }
+            }
+            hashResult = resultText;
+        }
+    }
 
     //Print results
     cout << "Word: " << word << endl;
