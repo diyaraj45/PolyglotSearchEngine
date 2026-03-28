a8ca2670cc273b0dbd6a3d0ba7b796c7cd5d2907
Grecia <you@example.com>
2026-03-27 20:12:49 -0400

    Bridge.py

---
+++ bridge.py
@@ -0,0 +1,28 @@
+#Import and use Flask to create the bridge between frontend and backend
+#request receives the data sent from website and jsonify is used to send the response back to the frontend in JSON format
+from flask import Flask, request, jsonify
+web = Flask(__name__) #create backend web program, __name__ is a built in Python value Flask it is used internally.
+#create a route
+@web.route("/")
+def home():
+    return "Bridge is working" #to check if working.
+#create a route which is called "/search" and accepts POST requests- to send the word to search for
+@web.route("/search", methods=["POST"])
+def search():
+    #get the JSON data sent from website
+    data = request.get_json()
+    word = data["word"] #extract the word to search for
+
+    #for now fake results
+    return jsonify({
+        "word": word,
+        "trieResult": "Fake Trie Result",
+        "trieTime": 123,
+        "hashResult": "Fake Hash Result",
+        "hashTime": 456
+    })
+
+#This is what starts backend when run.
+
+if __name__ == "__main__":
+    web.run(debug=True)
